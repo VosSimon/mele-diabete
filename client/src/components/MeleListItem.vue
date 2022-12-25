7962f0ed784fe1331702e3a891d5efc1c20f0968
Simon Vos <simonvos@SB-MCH-MBP-Simon.local>
2022-12-25 22:19:18 +0100

    feat(component): Create nutrient list component

---
+++ client/src/components/MeleListItem.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+import type { Category, Nutrient } from "@/types/types";
+
+export default defineComponent({
+  props: {
+    data: {
+      // provide more specific type to `Object`
+      type: Object as PropType<Category | Nutrient>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <li class="mele-list__item">{{ data.name }}</li>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss">
+$c: ".mele-list__item";
+#{$c} {
+  align-items: center;
+  background-color: var(--vt-c-indigo);
+  border-bottom: 1px solid var(--vt-c-white);
+  color: var(--vt-c-white);
+  cursor: pointer;
+  display: flex;
+  height: 45px;
+  padding: 0.5rem 1rem;
+
+  .mele-list--inset & {
+    background-color: var(--color-background);
+    border-bottom: 1px solid var(--vt-c-indigo);
+    color: var(--color-text);
+  }
+}
+</style>
--- client/src/views/MeleNutrients.vue
+++ client/src/views/MeleNutrients.vue
@@ -1,34 +1,86 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import type { PropType } from "vue";
-import type { Category } from "@/types/types";
+import type { Category, Maybe } from "@/types/types";
+import MeleListItem from "@/components/MeleListItem.vue";
 
 export default defineComponent({
   props: {
     data: {
-      // provide more specific type to `Object`
       type: Object as PropType<Array<Category>>,
       required: true,
     },
   },
+  data() {
+    return {
+      showingCategory: null as Maybe<number>,
+    };
+  },
+  components: { MeleListItem },
 });
 </script>
 
 <template>
   <div>
     <h1>Nutrients</h1>
-    <ul>
-      <li v-for="nutrient in data" :key="nutrient.name">{{ nutrient.name }}</li>
+    <ul class="mele-list">
+      <MeleListItem
+        @click="
+          showingCategory = showingCategory === catIndex ? null : catIndex
+        "
+        v-for="(category, catIndex) in data"
+        :key="`category-${catIndex}`"
+        :data="category"
+      >
+        <Transition name="roll-out" mode="out-in">
+          <ul
+            v-if="category.nutrients && showingCategory === catIndex"
+            class="mele-list mele-list--inset"
+          >
+            <MeleListItem
+              v-for="(nutrient, nutIndex) in category.nutrients"
+              :key="`nutrient-${nutIndex}`"
+              :data="nutrient"
+            />
+          </ul>
+        </Transition>
+      </MeleListItem>
     </ul>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+$list: ".mele-list";
+#{$list} {
+  list-style: none;
+  margin-left: 0;
+  padding-left: 0;
+
+  &--inset {
+    max-height: calc(10 * 45) + px;
+    overflow-y: auto;
+    padding-left: 1rem;
+  }
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
+}
+
+.roll-out-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.roll-out-leave-active {
+  transition: all 0.5s ease-in;
+}
+
+.roll-out-enter-from,
+.roll-out-leave-to {
+  max-height: 0px;
 }
 </style>
